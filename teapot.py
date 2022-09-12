c77e0564cf6b998b2518396ae7d6036595dba01a
Jang Rush <weakish@gmail.com>
2022-09-12 14:06:17 +0000

    :new: health check

--- teapot.py
+++ teapot.py
@@ -1,13 +1,19 @@
 from http import HTTPStatus
 from starlette.applications import Starlette
 from starlette.routing import Route
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, JSONResponse
 
 async def teapot(request):
     _ = request
     return FileResponse(status_code=HTTPStatus.IM_A_TEAPOT,
             path="joni-ludlow-rqaSSf7N3rc-unsplash.jpg")
 
+async def health(request):
+    _ = request
+    return JSONResponse({"status": "ok"})
+
 app = Starlette(routes=[
     Route('/', endpoint=teapot),
+    Route('/teapot', endpoint=teapot),
+    Route('/health', endpoint=health),
     ])
